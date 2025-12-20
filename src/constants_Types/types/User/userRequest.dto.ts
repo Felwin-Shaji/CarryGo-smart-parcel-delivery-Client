058c92762a2f7750f9add905eb8144d0561c52b0
Felwin Shaji <felwinshaji@gmail.com>
2025-12-21 00:03:36 +0530

    refactor(user): split user DTO into request and response models

---
+++ src/constants_Types/types/User/userRequest.dto.ts
@@ -0,0 +1,4 @@
+import type { BaseResetPasswordRequestDTO } from "../BaseTypes/baseTypes.dto";
+
+// user-profile resetpassword request dto
+export interface UserResetPasswordRequestDTO extends BaseResetPasswordRequestDTO { }
