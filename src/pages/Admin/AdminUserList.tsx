08a745362fdb457528ed5e1781f651f86c96d7ce
Felwin Shaji <felwinshaji@gmail.com>
2026-01-04 17:01:20 +0530

    feat(admin): improve user list and reusable table components

--- src/pages/Admin/AdminUserList.tsx
+++ src/pages/Admin/AdminUserList.tsx
@@ -4,8 +4,9 @@ import { DashboardProvider } from "../../context/DashboardProvider";
 import { DataTable } from "../../components/Table/Table";
 import toast from "react-hot-toast";
 import LoadingScreen from "../../components/loading/CarryGoLoadingScreen";
-import { useAdmin } from "../../Services/Admin";
+
 import { AdminUserListColumns } from "../../config/TableColumns/adminUserListTableColumn";
+import { useAdmin } from "../../Services/Admin/Admin";
 
 
 const AdminUserList = () => {
