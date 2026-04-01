4da449e42254a67da65545901f6dbeb0978b5b7a
Felwin Shaji <felwinshaji@gmail.com>
2026-04-01 11:10:48 +0530

    chore(app): update app initialization for shipment module

--- src/main.tsx
+++ src/main.tsx
@@ -6,15 +6,22 @@ import { BrowserRouter } from 'react-router-dom'
 import { Provider } from 'react-redux'
 import { store } from './store/store.ts'
 
+
+
 import { fixLeafletIcons } from './utils/fixLeafletIcons.ts'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 fixLeafletIcons();
+
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
-      <Provider store={store}>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <Provider store={store}>
           <App />
-      </Provider>
-    </BrowserRouter>
+        </Provider>
+      </BrowserRouter>
+    </QueryClientProvider>
   </StrictMode>
 )
